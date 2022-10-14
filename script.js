b3dcbeb9f2015f769d878a10e2856642db906755
firhanabdurr <firhanabdurr@gmail.com>
2022-10-14 07:50:34 +0700

    add file

---
+++ script.js
@@ -0,0 +1,45 @@
+let jawaban = '', soal = ''
+$(document).ready(() => {
+    $('.btn').click(() => {
+        jawaban = $('jawaban').val()
+        soal = $('soal').val()
+        switch (soal) {
+            case 'm1':
+                if (jawaban === "") {
+
+                } else {
+
+                }
+            case 'd2':
+                if (jawaban === "") {
+
+                } else {
+
+                }
+            case 'm3':
+                if (jawaban === "") {
+
+                } else {
+
+                }
+            case 'd4':
+                if (jawaban === "") {
+
+                } else {
+
+                }
+            case 'd5':
+                if (jawaban === "") {
+
+                } else {
+
+                }
+            case 'd6':
+                if (jawaban === "") {
+
+                } else {
+
+                }
+        }
+    })
+})
